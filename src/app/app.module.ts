94af63caa8c6ec0bb41361d726670a2a0594ca7e
keke <keke@cnblogs.com>
2018-04-11 11:37:58 +0800

    Sample dropdwon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DropdownModule } from '../../ngx-dropdown';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
